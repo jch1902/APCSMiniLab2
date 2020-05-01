0007c83b9ea0d59925684fe46fb59f9af7ab1326
AadilU <aadilu2002@gmail.com>
2020-05-01 14:18:34 -0700

    moved walls

--- APCSMinilab2.1/src/socialdistancing/Control.java
+++ APCSMinilab2.1/src/socialdistancing/Control.java
@@ -1,6 +1,7 @@
 package socialdistancing;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.ArrayList;
@@ -15,15 +16,15 @@ public class Control {
 		public int numInfected = 0;
 		public int numDied= 0;
 		
-		Wall vWall1 = Simulator.vWall1;
-		Wall vWall2 = Simulator.vWall2;
-		Wall vWall3 = Simulator.vWall3;
-		Wall vWall4 = Simulator.vWall4;
+		protected Wall vWall1 = new Wall(550, 0, "SocialDistancingImages/wall2.png", true);
+		protected Wall vWall2 = new Wall(200, 0, "SocialDistancingImages/wall2.png", true);
+		protected Wall vWall3 = new Wall(550, 400, "SocialDistancingImages/wall2.png", true);
+		protected Wall vWall4 = new Wall(200, 400, "SocialDistancingImages/wall2.png", true);
 		
-		Wall hWall1 = Simulator.hWall1;
-		Wall hWall2 = Simulator.hWall2;
-		Wall hWall3 = Simulator.hWall3;
-		Wall hWall4 = Simulator.hWall4;
+		protected Wall hWall1 = new Wall(620, 160, "SocialDistancingImages/wall1.png", false);
+		protected Wall hWall2 = new Wall(-25, 160, "SocialDistancingImages/wall1.png", false);
+		protected Wall hWall3 = new Wall(620, 400, "SocialDistancingImages/wall1.png", false);
+		protected Wall hWall4 = new Wall(-25, 400, "SocialDistancingImages/wall1.png", false);
 		
 		// simulation control values
 		public int  numPeople;			
@@ -159,6 +160,29 @@ public class Control {
 				index++;
 				
 			}
+		}
+		
+		public void paintWalls(Graphics g)
+		{
+			g.drawImage(vWall1.getImage(), vWall1.getX(), vWall1.getY(), null);
+			g.drawImage(vWall2.getImage(), vWall2.getX(), vWall2.getY(), null);
+			g.drawImage(vWall3.getImage(), vWall3.getX(), vWall3.getY(), null);
+			g.drawImage(vWall4.getImage(), vWall4.getX(), vWall4.getY(), null);
+			
+			//draws horizontal walls
+			g.drawImage(hWall1.getImage(), hWall1.getX(), hWall1.getY(), null);
+			g.drawImage(hWall2.getImage(), hWall2.getX(), hWall2.getY(), null);
+			g.drawImage(hWall3.getImage(), hWall3.getX(), hWall3.getY(), null);
+			g.drawImage(hWall4.getImage(), hWall4.getX(), hWall4.getY(), null);
+			
+			//sets text color
+			g.setColor(Color.BLACK);
+			g.setFont(new Font("Roboto", Font.BOLD, 25));
+			
+			g.drawString("Sprouts", 610, 50);
+			g.drawString("Scripps Medical", 5, 50);
+			g.drawString("Board and Brew", 5, 440);
+			g.drawString("Mr. M's House", 590, 440);
 		}
 
 		public void checkWallCollision(Person p) {
